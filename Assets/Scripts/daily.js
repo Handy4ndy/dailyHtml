8f9be878bf07c381615c36cd5a02a5047f8d08ad
Andy <110458000+Handy4ndy@users.noreply.github.com>
2023-09-10 14:37:15 +0100

    structure and files

---
+++ Assets/Scripts/daily.js
@@ -0,0 +1,16 @@
+// Get the current day of the week (0 = Sunday, 1 = Monday, ...)
+const currentDate = new Date();
+const currentDay = currentDate.getDay(); // Returns a number (0-6)
+
+// Define an array of day names corresponding to your HTML file names
+const dayNames = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+
+// Relative path to the HTML files
+const htmlFilePath = `Assets/Html/${dayNames[currentDay]}.html`;
+
+// Load the content for the current day
+fetch(htmlFilePath)
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('dailyContent').innerHTML = data;
+    });
